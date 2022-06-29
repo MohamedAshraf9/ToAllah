74444b29611f15aa47ae6e287491183f6efca163
Your Name <mohamedashraf1781998@gmail.com>
2022-06-29 15:39:48 +0200

    update

---
+++ app/src/main/java/com/megahed/eqtarebmenalla/common/Constants.kt
@@ -0,0 +1,5 @@
+package com.megahed.eqtarebmenalla.common
+
+object Constants {
+    const val BASE_URL = "https://http://api.aladhan.com"
+}
---
+++ app/src/main/java/com/megahed/eqtarebmenalla/feature_data/data/local/Convertor.kt
@@ -0,0 +1,8 @@
+package com.megahed.eqtarebmenalla.feature_data.data.local
+
+import com.megahed.eqtarebmenalla.feature_data.data.util.JsonParser
+
+class Convertor(
+    private val jsonParser: JsonParser
+) {
+}
