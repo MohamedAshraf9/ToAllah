5c89313b2fda041181c35f64ad09300e53da1d73
MohamedAshraf9 <mohamedashraf10665@gmail.com>
2023-03-21 14:34:16 +0200

    update

--- app/src/main/java/com/megahed/eqtarebmenalla/feature_data/data/remote/prayerTime/IslamicApi.kt
+++ app/src/main/java/com/megahed/eqtarebmenalla/feature_data/data/remote/prayerTime/IslamicApi.kt
@@ -15,3 +15,25 @@ interface IslamicApi {
 
     ): IslamicInfo
 }
+
+/*
+
+method
+ 0 - Shia Ithna-Ansari
+1 - University of Islamic Sciences, Karachi
+2 - Islamic Society of North America
+3 - Muslim World League
+4 - Umm Al-Qura University, Makkah
+5 - Egyptian General Authority of Survey
+7 - Institute of Geophysics, University of Tehran
+8 - Gulf Region
+9 - Kuwait
+10 - Qatar
+11 - Majlis Ugama Islam Singapura, Singapore
+12 - Union Organization islamic de France
+13 - Diyanet İşleri Başkanlığı, Turkey
+14 - Spiritual Administration of Muslims of Russia
+15 - Moonsighting Committee Worldwide (also requires shafaq parameter)
+16 - Dubai (unofficial)
+
+ */
