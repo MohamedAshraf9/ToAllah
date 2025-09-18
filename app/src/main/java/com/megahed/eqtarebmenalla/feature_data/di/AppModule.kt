2c33f469579edb6eefbc8786fe4b45f27d2b38df
Ahmed Nasser <ahmed.nasser.11097@gmail.com>
2025-09-18 19:23:52 +0300

    Refactor: Update Room database setup to use destructive migration fallback
    This commit modifies the Room database setup in `AppModule.kt`.
    
    - Removed explicit `addMigrations` calls (MIGRATION_3_4, MIGRATION_4_5, MIGRATION_5_6, MIGRATION_6_7, MIGRATION_7_8).
    - Added `fallbackToDestructiveMigration(false)` to handle schema changes by recreating the database if migrations are not provided. This is set to `false` to prevent data loss by default if a migration is missing.

--- app/src/main/java/com/megahed/eqtarebmenalla/feature_data/di/AppModule.kt
+++ app/src/main/java/com/megahed/eqtarebmenalla/feature_data/di/AppModule.kt
@@ -129,10 +129,9 @@ object AppModule {
             app,
             MyDatabase::class.java,
             MyDatabase.DATABASE_NAME)
-            .addMigrations( MIGRATION_3_4, MIGRATION_4_5,MIGRATION_5_6,MIGRATION_6_7, MIGRATION_7_8)
+            .fallbackToDestructiveMigration(false)
             .createFromAsset("prepopulated_db.db")
             .build()
-
     }
 
     @Provides
